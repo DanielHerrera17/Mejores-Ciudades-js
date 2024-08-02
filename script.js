f14be2efaf7ab598c34c9229ae115d92b868bcf9
Daniel Herrera <uh24001@ues.edu.sv>
2024-08-01 19:14:21 -0600

    Agregamos Javascript al proyecto de Mejores ciudades.js

---
+++ script.js
@@ -0,0 +1,42 @@
+import { barcelona, roma, paris, londres, sansalvador } from './ciudades.js';
+
+let enlaces = document.querySelectorAll('a');
+let tituloElemento = document.getElementById('titulo');
+let subTituloElemento = document.getElementById('subtitulo');
+let parrafoElemento = document.getElementById('parrafo');
+
+// Agregar un evento click a cada enlace
+
+enlaces.forEach(function (enlace) {
+    enlace.addEventListener('click', function () {
+        // Remover activo
+        enlaces.forEach(function (enlace) {
+            enlace.classList.remove('active');
+        });
+
+
+        // Agregar la clase "Active" al enlace actual
+        this.classList.add('active')
+
+        // Obtener el contenido correspondiente segun el enlace
+        let contenido = obtenerContenido(this.textContent)
+
+        // Mostrar el contenido en el DOM
+        tituloElemento.innerHTML = contenido.titulo;
+        subTituloElemento.innerHTML = contenido.subtitulo;
+        parrafoElemento.innerHTML = contenido.parrafo;
+    });
+});
+
+// Funcion para traer la informacion correcta desde ciudades.js
+
+function obtenerContenido(enlace) {
+    let contenido = {
+        'Barcelona': barcelona,
+        'Roma': roma,
+        'París': paris,
+        'Londres': londres,
+        'San Salvador': sansalvador
+    };
+    return contenido[enlace];
+}
